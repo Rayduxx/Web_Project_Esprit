b988b99458688eabbeade08f0cf0ff37a2c6c88b
bitri12 <60946412+bitri12@users.noreply.github.com>
2021-11-17 19:36:07 +0100

    reparation nom

--- view/includes/header.php
+++ view/includes/header.php
@@ -8,7 +8,7 @@ require("../controller/LogementC.php");
   <head>
     <meta charset="utf-8">
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
-    <title>Star City - <?php echo $page_titre;?></title>
+    <title>Sky City - <?php echo $page_titre;?></title>
     <link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	  <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 	  <link href="../../assets/css/style.css" rel="stylesheet">
