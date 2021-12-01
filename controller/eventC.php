691d21580a6ec7eefae2ba1c45467dd30becd6e9
fediza <83580868+fediza@users.noreply.github.com>
2021-12-01 10:06:25 +0100

    Update eventC.php

--- controller/eventC.php
+++ controller/eventC.php
@@ -15,6 +15,24 @@ function NombreEventsUser($u)
   $result = $query->rowCount();
   return $result;
 }
+function NombreEvents (){
+  $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', '');
+  $query = $bdd->prepare("SELECT * FROM event");
+  $query->execute();
+  $result = $query->rowCount();
+  return $result;
+}
 
+function nombrePlaceVide($id){
+  $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', '');
+  $query = $bdd->prepare("SELECT * FROM event WHERE id = ?");
+  $query->execute(array($id));
+  $evenement = $query->fetch();
+  $selectionP = $bdd->prepare("SELECT FROM participants WHERE eventId = ?");
+  $selectionP->execute(array($id));
+  $comptage = $selectionP->rowCount();
+  $res = $evenement['maxParticipant'] - $comptage;
+  return $res;
+}
 
  ?>
