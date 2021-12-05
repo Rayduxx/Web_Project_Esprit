446d6fdf814f9d99c6bff72fb5f2d0a7bab68baa
bitri12 <60946412+bitri12@users.noreply.github.com>
2021-12-05 11:42:55 +0100

    Update liste_entretient_agent.php

---
+++ view/liste_entretient_agent.php
@@ -0,0 +1,27 @@
+<?php include "./includes/profil_header.php"; ?>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<br/><br/><br/>
+<?php include "./includes/footer.php"; ?>
